61e776aac22b918d6e21b7909be3aced405ab347
Lukas Kluft <lukas.kluft@gmail.com>
2024-08-27 10:04:56 -0100

    Revise argument names and fix their internal use

--- ecscrape/tools.py
+++ ecscrape/tools.py
@@ -1,6 +1,6 @@
 import argparse
 import datetime
-import pathlib
+from pathlib import Path
 
 import xarray as xr
 
@@ -13,27 +13,28 @@ def main():
         description="Download, archive, remap, rechunk and store ECMWF forecasts.",
     )
     parser.add_argument("--time", "-t", type=str, default=None)
-    parser.add_argument("--cache", "-c", type=str)
-    parser.add_argument("--out", "-o", type=str)
+    parser.add_argument("--cache", "-c", type=Path)
+    parser.add_argument("--store", "-s", type=Path)
 
     args = parser.parse_args()
 
     if args.time is None:
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(datetime.timezone.utc)
         fctime = lib.get_latest_forecasttime(now)
     else:
         fctime = datetime.datetime.fromisoformat(args.time)
 
-    outdir = pathlib.Path(args.outdir)
-    outdir.mkdir(parents=True, exist_ok=True)
+    # Download GRIB2 files into cache (and build indices)
+    args.cache.mkdir(parents=True, exist_ok=True)
+    lib.download_forecast(fctime, outdir=args.cache)
 
-    lib.download_forecast(fctime, outdir=outdir)
-    datasets = lib.create_datasets(outdir=outdir)
+    # Create reference filesystems from indices
+    datasets = lib.create_datasets(outdir=args.cache)
 
+    # Merge datasets and convert to Zarr store
     ecmwf = xr.open_mfdataset(datasets, engine="zarr")
-
     lib.set_swift_token()
     lib.healpix_dataset(ecmwf).to_zarr(
-        args.out,
+        args.store,
         storage_options={"get_client": lib.get_client},
     )
